d668922242c3f021bafa4d5745327535ac430c07
Melinda Chirila <mchirila980@west-mec.org>
2019-02-21 14:17:46 -0700

    beautified the files 2/21/2019.

--- ja_quote.js
+++ ja_quote.js
@@ -23,18 +23,18 @@ quote.textContent = getQuote(randomQ);
 
 //this is the get quote function.
 function getQuote(n) {
-   var quotes = [
-   "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-   "I hate to hear you talk about all women as if they were fine ladies instead of rational creatures. None of us want to be in calm waters all our lives.",
-   "Silly things do cease to be silly if they are done by sensible people in an impudent way.",
-   "Give a girl an education and introduce her properly into the world, and ten to one but she has the means of settling well, without further expense to anybody.",
-   "Life seems but a quick succession of busy nothings.",
-   "Our scars make us know that our past was for real.",
-   "I cannot speak well enough to be unintelligible.",
-   "One cannot be always laughing at a man without now and then stumbling on something witty.",
-   "Men were put into the world to teach women the law of compromise.",
-   "The person, be it gentlemen or lady, who has not pleasure in a good novel, must be intolerably stupid."
-   ];
-   
-   return quotes[n];
+    var quotes = [
+        "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+        "I hate to hear you talk about all women as if they were fine ladies instead of rational creatures. None of us want to be in calm waters all our lives.",
+        "Silly things do cease to be silly if they are done by sensible people in an impudent way.",
+        "Give a girl an education and introduce her properly into the world, and ten to one but she has the means of settling well, without further expense to anybody.",
+        "Life seems but a quick succession of busy nothings.",
+        "Our scars make us know that our past was for real.",
+        "I cannot speak well enough to be unintelligible.",
+        "One cannot be always laughing at a man without now and then stumbling on something witty.",
+        "Men were put into the world to teach women the law of compromise.",
+        "The person, be it gentlemen or lady, who has not pleasure in a good novel, must be intolerably stupid."
+    ];
+
+    return quotes[n];
 }
